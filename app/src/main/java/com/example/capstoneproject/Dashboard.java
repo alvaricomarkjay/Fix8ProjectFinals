81e9c27100b585d4343c7c90462476a1ea86b7c0
Mark Jay Alvarico <68045820+alvaricomarkjay@users.noreply.github.com>
2022-09-22 11:56:36 +0800

    updated 22/09/2022

--- app/src/main/java/com/example/capstoneproject/Dashboard.java
+++ app/src/main/java/com/example/capstoneproject/Dashboard.java
@@ -2,11 +2,15 @@ package com.example.capstoneproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.capstoneproject.databinding.ActivityDashboardBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,36 +21,39 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Dashboard extends AppCompatActivity {
 
-    private TextView curUser;
-    private FirebaseDatabase firebaseDatabase;
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference;
-
-
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
-        curUser = findViewById(R.id.currentUser);
-        firebaseAuth = FirebaseAuth.getInstance();
+            switch (item.getItemId()){
 
-        databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fix8project-default-rtdb.firebaseio.com/");
-
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String name = (String)snapshot.child("users").child("fullname").getValue();
-                curUser.setText("Current User: " + name);
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
+            return true;
         });
 
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
